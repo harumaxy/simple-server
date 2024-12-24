d42a803d1878951d2401763238208ed0a7b80be8
harumaxy <harumaxy@gmail.com>
2024-12-24 13:00:20 +0900

    update

--- lib/simple_server.ex
+++ lib/simple_server.ex
@@ -1,12 +1,17 @@
 defmodule SimpleServer do
-  use Plug.Router
+  @behaviour Plug
 
-  plug(:parse_query)
-  plug(:auth)
-  plug(:set_content_type, "text/plain")
+  import Plug.Conn
 
-  plug(:match)
-  plug(:dispatch)
+  def init(options), do: options
+
+  def call(conn, _options) do
+    try do
+      conn |> parse_query() |> auth() |> set_content_type("text/plain") |> route()
+    catch
+      :unauthorized -> conn |> send_resp(401, "Unauthorized")
+    end
+  end
 
   def parse_query(conn, _opts \\ []) do
     queries = conn.query_string |> URI.decode_query()
@@ -16,20 +21,20 @@ defmodule SimpleServer do
   def auth(conn, _opts \\ []) do
     case conn.assigns[:queries] do
       %{"id" => "user", "password" => "password"} -> conn
-      _ -> conn |> send_resp(401, "Unauthorized") |> halt()
+      _ -> throw(:unauthorized)
     end
   end
 
+  @spec set_content_type(Plug.Conn.t(), binary()) :: Plug.Conn.t()
   def set_content_type(conn, type) do
     conn |> put_resp_header("content-type", type)
   end
 
-  get "/" do
-    IO.puts(conn.method)
-    send_resp(conn, 200, "Hello, world!")
-  end
-
-  match _ do
-    conn |> send_resp(404, "Path not found")
+  def route(conn, _opts \\ []) do
+    case {conn.method, conn.request_path} do
+      {"GET", "/"} -> conn |> send_resp(200, "Hello, world!")
+      {"GET", "/hello"} -> conn |> send_resp(200, "Hello, #{conn.assigns[:queries].id}!")
+      _ -> conn |> send_resp(404, "Path not found")
+    end
   end
 end
